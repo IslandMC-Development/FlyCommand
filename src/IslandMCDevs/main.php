cc0de8ab6660e36e95e3862c112c0fec264b903d
Grant Stahr <76236444+InsulatorGMan@users.noreply.github.com>
2021-06-13 14:34:48 -0500

    Update main.php

--- src/IslandMCDevs/main.php
+++ src/IslandMCDevs/main.php
@@ -12,7 +12,7 @@ use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\utils\TextFormat;
 
-class BlazinFly extends PluginBase implements Listener{
+class FlyCommand extends PluginBase implements Listener{
 
 	const PREFIX = TextFormat::AQUA . "Fly-Command" . TextFormat::GOLD . " > ";
 	const VERSION = "v1.0.1";
